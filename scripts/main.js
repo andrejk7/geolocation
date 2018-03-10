3c1fef66a511c0cebff1c68d4475982ce2da6f00
Andrej Kolarovski <andrej@gsix.me>
2018-03-10 15:00:20 +0100

    added target object

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,8 @@ var arrow = document.getElementById("arrow");
 var locationOutput = document.getElementById("loc");
 var userBearing;
 var userPosition;
+var target = {
+  //цементара Усје, ако некој го интригира
+  latitude: 41.9534282,
+  longitude: 21.4760617
+}
