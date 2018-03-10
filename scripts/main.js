53899cd367646fe6df3c8ad890c2eca2651b1215
Andrej Kolarovski <andrej@gsix.me>
2018-03-10 15:08:04 +0100

    added current target

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,23 @@
 var output = document.getElementById("out");
 var arrow = document.getElementById("arrow");
 var locationOutput = document.getElementById("loc");
+var directionOutput = document.getElementById("dir");
 var userBearing;
 var userPosition;
-var target = {
+var currentTarget = {
   //цементара Усје, ако некој го интригира
   latitude: 41.9534282,
   longitude: 21.4760617
 }
+
+function updateDirection() {
+  if (!userPosition) { return; }
+  var lat1  = userPosition.coords.latitude;
+  var lon1 = userPosition.coords.longitude;
+  var lat2 = currentTarget.latitude;
+  var lon2 = currentTarget.longitude;
+  var bear = bearing(lat1, lon1, lat2, lon2);
+  directionOutput.innerHTML = '<p><strong>Direction: ' + bear + '</p></strong>';
+}
+
+setInterval(updateDirection, 1000);
