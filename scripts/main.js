8668e8cde6a10713721cccc30b87ea9e50edbbaf
Andrej Kolarovski <andrej@gsix.me>
2018-03-11 12:17:54 +0100

    added display in meters, threshold, arrival message

--- scripts/main.js
+++ scripts/main.js
@@ -13,16 +13,23 @@ var currentTarget = {
 var trueBearing;
 var waypoints = [];
 var selectedWaypointIndex = -1;
+var distThresh = 5; //5 meters threshold
 
 function updateDirection() {
   if (!userPosition) { return; }
+  if (selectedWaypointIndex === waypoints.length) {
+    return directionOutput.innerHTML = '<p><strong>You have arrived</strong></p>';
+  }
   var lat1  = userPosition.coords.latitude;
   var lon1 = userPosition.coords.longitude;
   var lat2 = selectedWaypointIndex > -1 ? waypoints[selectedWaypointIndex].latitude : currentTarget.latitude;
   var lon2 = selectedWaypointIndex > -1 ? waypoints[selectedWaypointIndex].longitude : currentTarget.longitude;
   trueBearing = bearing(lat1, lon1, lat2, lon2);
   var d = distance(lat1, lon1, lat2, lon2);
-  directionOutput.innerHTML = '<p><strong>Distance: ' + d + '</p></strong>' +
+  if (d * 1000 < distThresh) {
+    selectedWaypointIndex++;
+  }
+  directionOutput.innerHTML = '<p><strong>Distance: ' + d * 1000 + ' m</p></strong>' +
                               '<p><strong>Target: lon ' + lon2 + ' lat ' + lat2 + '</p></strong>' +
                               '<p><strong>Waypoint index: ' + selectedWaypointIndex + '</p></strong>';
 }
